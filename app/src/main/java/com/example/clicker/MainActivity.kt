39048513e185c3a2c195dbd6ff0a226d14c72380
Danbug <unwantedmouth@gmail.com>
2023-02-25 18:24:21 +0200

    Added falling cookie animation on button Click

--- app/src/main/java/com/example/clicker/MainActivity.kt
+++ app/src/main/java/com/example/clicker/MainActivity.kt
@@ -1,32 +1,44 @@
 package com.example.clicker
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.os.SharedMemory
-import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     // save data
     private lateinit var shared: SharedPreferences
     private lateinit var button: ImageButton
     private lateinit var scoreText: TextView
+    // spawned cookies
+    private lateinit var newView: ImageView
+    private lateinit var mainLayout: ConstraintLayout
+    private lateinit var rng: Random
+
+
     private var score: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        shared = getSharedPreferences("ClickerDB", Context.MODE_PRIVATE)
+        mainLayout = findViewById(R.id.mainLayout)
 
+        shared = getSharedPreferences("ClickerDB", Context.MODE_PRIVATE)
         // maybe i should put it in OnResume or OnContinue
         readData()
 
         scoreText = findViewById(R.id.scoreText)
         scoreText.text =  getString(R.string.player_score, score.toString())
+
+        rng = Random(System.currentTimeMillis())
 
         button = findViewById(R.id.imageButton)
 
@@ -34,21 +46,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun buttonOnClickAddScore(){
-        score++;
-        scoreText.text =  getString(R.string.player_score, score.toString())
-
+        increaseScoreOnClick()
+        spawnCookie()
         // maybe i should put it in other place
         saveData()
     }
 
+    private fun increaseScoreOnClick(){
+        score++
+        scoreText.text =  getString(R.string.player_score, score.toString())
+    }
+    private fun spawnCookie(){
+
+        newView = ImageView(this)
+
+        mainLayout.addView(newView)
+
+        newView.layoutParams.height = 200
+        newView.layoutParams.width = 200
+        newView.x = rng.nextFloat() * 1000
+        newView.y = -300F
+        newView.alpha = 0.5F;
+        newView.setImageDrawable(getDrawable(R.drawable.cookie))
+        // move cookie
+        ObjectAnimator.ofFloat(newView, "translationY", 2080f).apply {
+            duration = 2000
+            start()
+        }
+    }
+
     private fun readData(){
-        score = shared.getInt("score",score);
+        score = shared.getInt("score",score)
     }
 
     private fun saveData(){
-        val editData = shared.edit();
-        editData.putInt("score",score);
-        editData.apply();
+        val editData = shared.edit()
+        editData.putInt("score",score)
+        editData.apply()
     }
 
 }
